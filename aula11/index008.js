b170e1c68e5015a7e1a717df501268b97775b2cb
Thalestata <118277842+Thalestata@users.noreply.github.com>
2022-12-21 09:37:41 -0300

    atualização

---
+++ aula11/index008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log( `A velocidade do seu carro é ${vel}km/h` )
+if(vel > 60){
+    console.log('Você ultrapassou o limete de velocidade. MULTADO!')
+}
+console.log('Dirija sempres usando cinto de segurança!')
