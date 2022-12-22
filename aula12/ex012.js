f3c18380bf282fa7f96ada059972106d59c9b470
Thalestata <118277842+Thalestata@users.noreply.github.com>
2022-12-22 10:17:44 -0300

    atualização

---
+++ aula12/ex012.js
@@ -0,0 +1,10 @@
+let agora = new Date()
+let hora = agora.getHours()
+console.log(`Agora são ${hora} horas`)
+if( hora <12){
+    console.log('Bom dia')
+}else if(hora <= 18){
+    console.log('boa tarde')
+}else{
+   console.log('Boa noite')
+}
